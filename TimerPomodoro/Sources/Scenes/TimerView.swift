e957d3208e9648ca49eafff5f1cc3a21f8a01bee
Ivan Sokol <sokolivan34@mail.ru>
2022-05-19 21:52:14 +0300

    Added logic and setup animation.

--- TimerPomodoro/Sources/Scenes/TimerView.swift
+++ TimerPomodoro/Sources/Scenes/TimerView.swift
@@ -9,22 +9,48 @@ import UIKit
 
 class TimerView: UIViewController {
 
+    private var timer = Timer()
+    private var durationTimer = Metric.workTimeValue {
+        didSet {
+            labelTimer.text = durationTimer < Metric.workTimeValue ? "00:0\(durationTimer)" : "00:\(durationTimer)"
+        }
+
+    }
+    private var isStarted = false {
+        didSet {
+            button.isSelected = isStarted
+        }
+    }
+    private var isWorkTime = true {
+        didSet {
+            durationTimer = isWorkTime ? Metric.workTimeValue : Metric.relaxTimeValue
+            labelTimer.textColor = isWorkTime ? Color.workState : Color.relaxState
+            setButtonColor()
+        }
+    }
+
     private lazy var labelTimer: UILabel = {
         var labelTimer = UILabel()
-        labelTimer.text = "25:00"
-        labelTimer.textColor = .black
-        labelTimer.font = .systemFont(ofSize: 80)
+        labelTimer.text = String(format: "%02i:%02i", Metric.minutes, Metric.seconds)
+        labelTimer.textColor = isWorkTime ? Color.workState : Color.relaxState
+        labelTimer.font = .systemFont(ofSize: Metric.labelSize)
         labelTimer.sizeToFit()
         return labelTimer
     }()
 
     private lazy var button: UIButton = {
+        let currentColor = isWorkTime ? Color.workState : Color.relaxState
         var button = UIButton(type: .system)
-        let configurationImage = UIImage.SymbolConfiguration(pointSize: 60)
-        let imageButton = UIImage(systemName: "play", withConfiguration: configurationImage)
-        button.setImage(imageButton, for: .normal)
+        let configurationImage = UIImage.SymbolConfiguration(pointSize: Metric.buttonSize)
+        button.tintColor = .clear
+        button.setPreferredSymbolConfiguration(configurationImage, forImageIn: .normal)
+        button.setImage(Icons.play?.withTintColor(currentColor, renderingMode: .alwaysOriginal), for: .normal)
+        button.setImage(Icons.pause?.withTintColor(currentColor, renderingMode: .alwaysOriginal), for: .selected)
+        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
         return button
     }()
+
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,8 +77,57 @@ class TimerView: UIViewController {
 
     }
 
+    private func setButtonColor() {
+        let currentColor = isWorkTime ? Color.workState : Color.relaxState
+        let playIcon = Icons.play?.withTintColor(currentColor, renderingMode: .alwaysOriginal)
+        let pauseIcon = Icons.pause?.withTintColor(currentColor, renderingMode: .alwaysOriginal)
+        button.setImage(playIcon, for: .normal)
+        button.setImage(pauseIcon, for: .selected)
+    }
 
+    // MARK: - Actions
+
+    @objc private func timerAction() {
+        guard durationTimer > 0  else {
+            isStarted = !isStarted
+            isWorkTime = !isWorkTime
+            timer.invalidate()
+            return
+        }
+
+        durationTimer -= 1
+    }
+
+    @objc private func buttonAction() {
+        isStarted = !isStarted
+
+        if isStarted {
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
+        } else {
+            timer.invalidate()
+        }
+    }
 
 
 }
 
+extension TimerView {
+    enum Icons {
+        static let pause = UIImage(systemName: "pause")
+        static let play = UIImage(systemName: "play")
+    }
+
+    enum Metric {
+        static let labelSize: CGFloat = 100
+        static let buttonSize: CGFloat = 80
+        static let workTimeValue = 10
+        static let relaxTimeValue = 5
+        static let minutes = workTimeValue / 60 % 60
+        static let seconds = workTimeValue % 60
+    }
+
+    enum Color {
+        static let workState = UIColor.black
+        static let relaxState = UIColor.white
+    }
+}
